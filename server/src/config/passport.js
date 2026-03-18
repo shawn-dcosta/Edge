e890d029d0978789ec6762ba4022c9aa2c61c1d2
shawn-dcosta <dcostashawn@gmail.com>
2026-03-18 17:49:54 +0530

    OAuth and Render configuration

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -7,7 +7,8 @@ import User from '../models/User.js';
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: "/api/auth/google/callback",
+    proxy: true
   },
   async (accessToken, refreshToken, profile, done) => {
     try {

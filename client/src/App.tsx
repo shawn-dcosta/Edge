b7c9ea2ff1bcb18f366113b0543f44d0f66d28c6
shawn-dcosta <dcostashawn@gmail.com>
2026-03-18 15:55:04 +0530

    Scrolling issue fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
@@ -16,6 +17,7 @@ function App() {
     <AuthProvider>
       <Toaster position="bottom-right" reverseOrder={false} />
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
